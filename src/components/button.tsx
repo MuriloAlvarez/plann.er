287de87bb45a8efd1fc08d72db550ffacf41f232
MuriloAlvarez <murilo12alvarez@gmail.com.br>
2024-11-09 19:38:23 -0300

    fix: 🐛 Corrigindo Identação

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,34 +2,41 @@ import { ComponentProps, ReactNode } from "react";
 import { tv, VariantProps } from "tailwind-variants";
 // 1:19:00
 const buttonVariants = tv({
-    base: 'font-medium rounded-lg px-5 flex items-center gap-2 justify-center',
+  base: "font-medium rounded-lg px-5 flex items-center gap-2 justify-center",
 
-    variants:{
-        variant:{
-            primary: 'bg-lime-400 text-lime-950 hover:bg-lime-300',
-            secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
-        },
-
-        size:{
-            default:'py-2',
-            full:'w-full h-11'
-        },
+  variants: {
+    variant: {
+      primary: "bg-lime-400 text-lime-950 hover:bg-lime-300",
+      secondary: "bg-zinc-800 text-zinc-200 hover:bg-zinc-700",
     },
 
-    defaultVariants:{
-        variant: 'primary',
-        size: 'default'
-    }
-})
+    size: {
+      default: "py-2",
+      full: "w-full h-11",
+    },
+  },
 
-interface ButtonProps extends ComponentProps<'button'>, VariantProps<typeof buttonVariants>{
-    children: ReactNode
+  defaultVariants: {
+    variant: "primary",
+    size: "default",
+  },
+});
+
+interface ButtonProps
+  extends ComponentProps<"button">,
+    VariantProps<typeof buttonVariants> {
+  children: ReactNode;
 }
 
-export function Button({children, variant, size, ...props}: ButtonProps) {
-    return(
-        <button {...props} className={buttonVariants({variant, size})}>
-            {children}
-        </button>
-    )
+export function Button({
+  children,
+  variant,
+  size,
+  ...props
+}: Readonly<ButtonProps>) {
+  return (
+    <button {...props} className={buttonVariants({ variant, size })}>
+      {children}
+    </button>
+  );
 }
--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,91 +1,86 @@
 // Importação de Ícones
-import { FormEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { InviteGuestsModal } from './invite-guests-modal';
-import { ConfirmTripModal } from './confirm-trip-modal';
-import { DestinationAndDateStep } from './steps/destination-and-date-step';
-import { InviteGuestsStep } from './steps/invite-guests-step';
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "./confirm-trip-modal";
+import { DestinationAndDateStep } from "./steps/destination-and-date-step";
+import { InviteGuestsStep } from "./steps/invite-guests-step";
 
 export function CreateTripPage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-// Criando Estados
-  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
-  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
-  const [emailsToInvite, setEmailsToInvite] = useState([
-    'user@gmail.com'
-  ])
-  const [isConfirmTripModal, setIsConfirmTripModal] = useState(false)
+  // Criando Estados
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [emailsToInvite, setEmailsToInvite] = useState(["user@gmail.com"]);
+  const [isConfirmTripModal, setIsConfirmTripModal] = useState(false);
 
-  function openGuestsInput(){
-    setIsGuestsInputOpen(true)
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true);
   }
 
-  function closeGuestsInput(){
-    setIsGuestsInputOpen(false)
-  }
-  
-  function openGuestsModal(){
-    setIsGuestsModalOpen(true)
-  }
-  
-  function closeGuestsModal(){
-  setIsGuestsModalOpen(false)
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false);
   }
 
-  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>){
-    event.preventDefault()
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true);
+  }
 
-    const data = new FormData(event.currentTarget)
-    const email = data.get('email')?.toString()
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false);
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email")?.toString();
 
     if (!email) {
-      return
+      return;
     }
 
-    if(emailsToInvite.includes(email)){
-      return
+    if (emailsToInvite.includes(email)) {
+      return;
     }
-    setEmailsToInvite([
-      ...emailsToInvite,
-      email
-    ])
+    setEmailsToInvite([...emailsToInvite, email]);
 
-    
-    event.currentTarget.reset()
+    event.currentTarget.reset();
   }
 
-  function removeEmailFromInvites(emailToRemove:string){
-    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+  function removeEmailFromInvites(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(
+      (email) => email !== emailToRemove
+    );
 
-    setEmailsToInvite(newEmailList)
+    setEmailsToInvite(newEmailList);
   }
 
-  function openConfirmTripModal(){
-    setIsConfirmTripModal(true)
-  }
-  
-  function closeConfirmTripModal(){
-    setIsConfirmTripModal(false)
+  function openConfirmTripModal() {
+    setIsConfirmTripModal(true);
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>){
-    event.preventDefault()
+  function closeConfirmTripModal() {
+    setIsConfirmTripModal(false);
+  }
 
-    navigate('./trips/123')
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    navigate("./trips/123");
   }
 
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
-        <div className='flex flex-col items-center gap-4'>
-          <img className='h-16' src="/logo.svg" alt="plann.er"/>
+        <div className="flex flex-col items-center gap-4">
+          <img className="h-16" src="/logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-xl">
             Convide seus amigos e planeje sua próxima viagem!
           </p>
         </div>
-        <div className='space-y-4'>
-          
+        <div className="space-y-4">
           <DestinationAndDateStep
             closeGuestsInput={closeGuestsInput}
             isGuestsInputOpen={isGuestsInputOpen}
@@ -94,11 +89,11 @@ export function CreateTripPage() {
 
           {isGuestsInputOpen && (
             <InviteGuestsStep
-            openGuestsModal={openGuestsModal}
-            emailsToInvite={emailsToInvite}
-            openConfirmTripModal={openConfirmTripModal}
+              openGuestsModal={openGuestsModal}
+              emailsToInvite={emailsToInvite}
+              openConfirmTripModal={openConfirmTripModal}
             />
-          )}   
+          )}
         </div>
 
         <p className="text-md text-zinc-500">
@@ -106,30 +101,30 @@ export function CreateTripPage() {
           <br /> com nossos
           <a className="text-zinc-300 underline" href="#">
             termos de uso
-          </a>{" "}
-          e{" "}
+          </a>
+          e
           <a className="text-zinc-300 underline" href="#">
             políticas de privacidade
           </a>
         </p>
       </div>
 
-{/* Abrir Modal de Convidados */}
+      {/* Abrir Modal de Convidados */}
       {isGuestsModalOpen && (
         <InviteGuestsModal
-        addNewEmailToInvite={addNewEmailToInvite}
-        closeGuestsModal={closeGuestsModal}
-        emailsToInvite={emailsToInvite}
-        removeEmailFromInvites={removeEmailFromInvites}
-        />  
+          addNewEmailToInvite={addNewEmailToInvite}
+          closeGuestsModal={closeGuestsModal}
+          emailsToInvite={emailsToInvite}
+          removeEmailFromInvites={removeEmailFromInvites}
+        />
       )}
 
-     {isConfirmTripModal && (
-      <ConfirmTripModal
-      closeConfirmTripModal={closeConfirmTripModal}
-      createTrip={createTrip}/>
-     )}
-    
+      {isConfirmTripModal && (
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
+      )}
     </div>
   );
 }
